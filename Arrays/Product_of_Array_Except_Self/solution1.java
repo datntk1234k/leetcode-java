99453ba3306375dc6a3e36d322592edd590eaf9b
Nguyen Thanh Dat <119286333+datntk1234k@users.noreply.github.com>
2025-09-12 17:07:56 +0700

    Update_Arrays

--- Arrays/Product_of_Array_Except_Self/solution1.java
+++ Arrays/Product_of_Array_Except_Self/solution1.java
@@ -3,12 +3,19 @@ package product_of_array_except_self;
 public class solution1 {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length();
-        int[] ans = new int[n];  
+        int[] ans = new int[n]; // array save output
 
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            ans = prefix;
-            prefix *= nums[i];
+        prefix = 1; // Dùng số 1 = đúng → vì “không có gì để nhân” nên lấy 1.
+        for (int i = 0; i <= n; i++) {
+            ans[i] = prefix;
+            prefix *= nums[i]; 
         }
+        postfix = 1; 
+        for (int i = n -1; i >= 0; i--) {
+            ans[i] = ans[i] * postfix;
+            postfix *= nums[i]; 
+        } return ans;
     }
 }
+
+ 
